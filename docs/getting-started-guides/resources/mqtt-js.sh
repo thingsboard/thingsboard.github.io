171c1a511d4e4b2efb1dcbd2a006cfceae2fc767
Andrew Shvayka <ashvayka@thingsboard.io>
2016-12-26 20:06:08 +0200

    Mqtt.js for Windows

--- docs/getting-started-guides/resources/mqtt-js.sh
+++ docs/getting-started-guides/resources/mqtt-js.sh
@@ -1,4 +1,18 @@
-# Publish serial number and firmware version attributes
-cat attributes-data.json | mqtt pub -v -h "$THINGSBOARD_HOST" -t "v1/devices/me/attributes" -u '$ACCESS_TOKEN' -s
-# Publish timeseries data as an object without timestamp (server-side timestamp will be used)
-cat telemetry-data.json | mqtt pub -v -h "$THINGSBOARD_HOST" -t "v1/devices/me/telemetry" -u '$ACCESS_TOKEN' -s
+#!/bin/sh
+
+# Set Thingsboard host to "demo.thingsboard.io" or "localhost"
+export THINGSBOARD_HOST=demo.thingsboard.io
+
+# Replace YOUR_ACCESS_TOKEN with one from Device credentials window.
+export ACCESS_TOKEN=YOUR_ACCESS_TOKEN
+
+# Read serial number and firmware version attributes
+ATTRIBUTES=$( cat attributes-data.json )
+export ATTRIBUTES
+
+# Read timeseries data as an object without timestamp (server-side timestamp will be used)
+TELEMETRY=$( cat telemetry-data.json )
+export TELEMETRY
+
+# publish attributes and telemetry data via mqtt client
+node publish.js
