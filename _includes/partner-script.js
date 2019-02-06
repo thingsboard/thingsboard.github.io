e098034e30449acc8b9fd2ece660381b6556736f
Igor Kulikov <ikulikov@thingsboard.io>
2019-02-06 13:13:45 +0200

    Hardware Partner Program

--- _includes/partner-script.js
+++ _includes/partner-script.js
@@ -1,16 +1,10 @@
-;(function () {
-	var partners = [
-		/*{
-			type: 0,
-			name: 'name',
-			logo: 'logo',
-			link: 'https://some-link.com/',
-			blurb: 'Some description.'
-		}*/
-	]
+{% capture partners %}{% include_relative partners.json %}{% endcapture %}
 
-	var isvContainer = document.getElementById('isvContainer')
-	var servContainer = document.getElementById('servContainer')
+;(function () {
+	var partners = {{ partners }};
+
+	var hardwareContainer = document.getElementById('hardwareContainer')
+	var integratorContainer = document.getElementById('integratorContainer')
 
 	var sorted = partners.sort(function (a, b) {
 		if (a.name > b.name) return 1
@@ -19,29 +13,91 @@
 	})
 
 	sorted.forEach(function (obj) {
-		var box = document.createElement('div')
-		box.className = 'partner-box'
+		var box = document.createElement('div');
+		box.className = 'partner-box';
 
-		var img = document.createElement('img')
-		img.src = '/images/square-logos/' + obj.logo + '.png'
+		if (obj.program) {
+            var programImg = document.createElement('img');
+            programImg.className = 'partner-program';
+            programImg.src = '/images/partners/' + obj.program + '-partner.svg';
+            box.appendChild(programImg);
+        }
 
-		var div = document.createElement('div')
+		var img = document.createElement('img');
+        img.className = 'logo';
+		img.src = '/images/partners/' + obj.logo;
 
-		var p = document.createElement('p')
-		p.textContent = obj.blurb
+		var div = document.createElement('div');
 
-		var link = document.createElement('a')
-		link.href = obj.link
-		link.target = '_blank'
-		link.textContent = 'Learn more'
+        var titleElement = document.createElement('p');
+        titleElement.textContent = obj.name;
+        titleElement.className = 'title';
 
-		div.appendChild(p)
-		div.appendChild(link)
+		var p = document.createElement('p');
+		p.textContent = obj.blurb;
 
-		box.appendChild(img)
-		box.appendChild(div)
+        var spacer = document.createElement('p');
+        p.style.flex = '1';
 
-		var container = obj.type ? servContainer : isvContainer
-		container.appendChild(box)
-	})
+        var linksElement = document.createElement('div');
+        linksElement.className = 'links';
+
+        for (var linkName in obj.links) {
+            var linkInfo = obj.links[linkName];
+            var link = document.createElement('a');
+            link.href = linkInfo.href;
+            if (linkInfo.target) {
+                link.target = linkInfo.target;
+            }
+            link.textContent = linkName;
+            if (linksElement.childNodes.length) {
+                var divider = document.createElement('span');
+                divider.textContent = ' | ';
+                linksElement.appendChild(divider);
+            }
+            linksElement.appendChild(link);
+        }
+
+        div.appendChild(titleElement);
+		div.appendChild(p);
+        div.appendChild(spacer);
+		div.appendChild(linksElement);
+
+		box.appendChild(img);
+		box.appendChild(div);
+
+		var container = obj.type === 'hardware' ? hardwareContainer : integratorContainer;
+		container.appendChild(box);
+	});
+
+	var becomeHardwarePartnerBox = document.createElement('div');
+    becomeHardwarePartnerBox.className = 'partner-box';
+
+    var div = document.createElement('div');
+    var p = document.createElement('p');
+    p.textContent = 'Want to become ThingsBoard Hardware Partner?';
+    div.appendChild(p);
+
+    var linksElement = document.createElement('div');
+    linksElement.className = 'links';
+
+    var a = document.createElement('a');
+    a.textContent = 'Apply for Hardware Partner Program';
+    a.href = '/partners/hardware-program';
+    linksElement.appendChild(a);
+
+
+    div.appendChild(linksElement);
+    becomeHardwarePartnerBox.appendChild(div);
+
+    /*var buttonElement = document.createElement('a');
+    buttonElement.className = 'button';
+    buttonElement.textContent = 'Become Hardware Partner';
+    buttonElement.href = '/partners/register';
+
+    becomeHardwarePartnerBox.appendChild(buttonElement);*/
+
+    hardwareContainer.appendChild(becomeHardwarePartnerBox);
+
+
 })();
