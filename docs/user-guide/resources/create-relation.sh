a4f3aaa6b83c566ea0d760e5a7d4989b4083b0eb
vlad <vponojko@gmail.com>
2020-02-06 17:59:31 +0200

    create-relation.sh fixed

--- docs/user-guide/resources/create-relation.sh
+++ docs/user-guide/resources/create-relation.sh
@@ -1,3 +1,3 @@
-curl -v -X POST -d @create-asset.json http://localhost:8080/api/relation \
+curl -v -X POST -d @create-relation.json http://localhost:8080/api/relation \
 --header "Content-Type:application/json" \
 --header "X-Authorization: $JWT_TOKEN"
