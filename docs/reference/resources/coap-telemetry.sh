9565927d53cfc3c96d8d7d9830d7a3eb7165c9a4
Viacheslav Kukhtyn <vkukhtyn@thingsboard.io>
2020-10-20 11:37:41 +0300

    Fix copy-paste text in script comments

--- docs/reference/resources/coap-telemetry.sh
+++ docs/reference/resources/coap-telemetry.sh
@@ -2,5 +2,5 @@
 cat telemetry-data-as-object.json | coap post coap://localhost/api/v1/$ACCESS_TOKEN/telemetry
 # Publish data as an array of objects without timestamp (server-side timestamp will be used)
 cat telemetry-data-as-array.json | coap post coap://localhost/api/v1/$ACCESS_TOKEN/telemetry
-# Publish data as an object with timestamp (server-side timestamp will be used)
+# Publish data as an object with timestamp (telemetry timestamp will be used)
 cat telemetry-data-with-ts.json | coap post coap://localhost/api/v1/$ACCESS_TOKEN/telemetry
--- docs/reference/resources/mosquitto-telemetry.sh
+++ docs/reference/resources/mosquitto-telemetry.sh
@@ -2,5 +2,5 @@
 mosquitto_pub -d -h "127.0.0.1" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -f "telemetry-data-as-object.json"
 # Publish data as an array of objects without timestamp (server-side timestamp will be used)
 mosquitto_pub -d -h "127.0.0.1" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -f "telemetry-data-as-array.json"
-# Publish data as an object with timestamp (server-side timestamp will be used)
+# Publish data as an object with timestamp (telemetry timestamp will be used)
 mosquitto_pub -d -h "127.0.0.1" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -f "telemetry-data-with-ts.json"
