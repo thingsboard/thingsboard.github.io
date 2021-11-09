dc8bfd0125ee5d13bf0a31552231415a50207e71
Volodymyr Babak <volodymyr.babak@gmail.com>
2021-11-09 16:23:56 +0200

    Remove unused files

--- docs/edge/reference/resources/get-token.sh
+++
@@ -1,1 +0,0 @@
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"username":"tenant@thingsboard.org", "password":"tenant"}' 'http://THINGSBOARD_URL/api/auth/login'
--- docs/edge/user-guide/resources/mqtt-ssl-configuration-run-twowaysslmqttclient.sh
+++
@@ -1,1 +0,0 @@
-python3 two-way-ssl-mqtt-client.py
