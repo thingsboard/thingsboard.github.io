94a71099570fee461dd94f539a05c9787273d05b
Andrii Shvaika <ashvayka@thingsboard.io>
2021-11-17 10:41:37 +0200

    Change MQTT and CoAP hosts for ThingsBoard Cloud

--- docs/pe/reference/resources/mosquitto-telemetry.sh
+++ docs/pe/reference/resources/mosquitto-telemetry.sh
@@ -2,13 +2,13 @@
 # for ThingsBoard Cloud
 
 # Publish data as an object without timestamp (server-side timestamp will be used)
-mosquitto_pub -d -q 1 -h "thingsboard.cloud" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -m "{"temperature":42}"
+mosquitto_pub -d -q 1 -h "mqtt.thingsboard.cloud"-t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -m "{"temperature":42}"
 # Publish data as an object without timestamp (server-side timestamp will be used) using data from file
-mosquitto_pub -d -q 1 -h "thingsboard.cloud" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -f "telemetry-data-as-object.json"
+mosquitto_pub -d -q 1 -h "mqtt.thingsboard.cloud"-t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -f "telemetry-data-as-object.json"
 # Publish data as an array of objects without timestamp (server-side timestamp will be used)  using data from file
-mosquitto_pub -d -q 1 -h "thingsboard.cloud" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -f "telemetry-data-as-array.json"
+mosquitto_pub -d -q 1 -h "mqtt.thingsboard.cloud"-t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -f "telemetry-data-as-array.json"
 # Publish data as an object with timestamp (telemetry timestamp will be used)  using data from file
-mosquitto_pub -d -q 1 -h "thingsboard.cloud" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -f "telemetry-data-with-ts.json"
+mosquitto_pub -d -q 1 -h "mqtt.thingsboard.cloud"-t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -f "telemetry-data-with-ts.json"
 
 # for local ThingsBoard
 
