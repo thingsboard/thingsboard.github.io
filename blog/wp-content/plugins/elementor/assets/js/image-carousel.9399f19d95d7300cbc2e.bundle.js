daceb902ec4b43d9499fdc9ca120a0e3031c607b
Sergey Matvienko <smatvienko@thingsboard.io>
2024-03-04 14:36:22 +0100

    blog update
    Signed-off-by: Sergey Matvienko <smatvienko@thingsboard.io>

---
+++ blog/wp-content/plugins/elementor/assets/js/image-carousel.9399f19d95d7300cbc2e.bundle.js
@@ -0,0 +1,29 @@
+/*! elementor - v3.19.0 - 07-02-2024 */
+"use strict";
+(self["webpackChunkelementor"] = self["webpackChunkelementor"] || []).push([["image-carousel"],{
+
+/***/ "../assets/dev/js/frontend/handlers/image-carousel.js":
+/*!************************************************************!*\
+  !*** ../assets/dev/js/frontend/handlers/image-carousel.js ***!
+  \************************************************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+
+
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+exports["default"] = void 0;
+class ImageCarousel extends elementorModules.frontend.handlers.CarouselBase {
+  getDefaultSettings() {
+    const settings = super.getDefaultSettings();
+    settings.selectors.carousel = '.elementor-image-carousel-wrapper';
+    return settings;
+  }
+}
+exports["default"] = ImageCarousel;
+
+/***/ })
+
+}]);
+//# sourceMappingURL=image-carousel.9399f19d95d7300cbc2e.bundle.js.map
