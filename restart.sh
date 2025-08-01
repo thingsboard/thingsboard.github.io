2cf121eeac2f243b9d8314e6e01859e6dece0c08
vparomskiy <paromskiy@gmail.com>
2025-08-01 19:38:22 +0300

    fix long prod build (with skipFindProductUrl = false)

--- restart.sh
+++ restart.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rm -rf _site .jekyll-metadata && bundle exec jekyll serve --config _config.yml,_config_dev.yml --host 0.0.0.0
+rm -rf _site .jekyll-metadata &&  ruby _scripts/generate_url_map.rb && bundle exec jekyll serve --config _config.yml --host 0.0.0.0
